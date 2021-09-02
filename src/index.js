69cca59b8c0d811ed7ab6b3aa6f5df01cd5ced49
eduardo <jocdwardo25@gmail.com>
2021-09-02 13:35:10 -0500

    contador manejado utilizando un solo estado (optimizacion de rendimiento)

--- src/index.js
+++ src/index.js
@@ -18,61 +18,40 @@ const App = () => {
   const [right, setRight] = useState(10)
 */
 
-  const INITIAL_COUNTER_STATE = {
-    left: 2,
-    right: 4,
-    mensaje: 'mensaje de estado'
-  }
-
-  const [counters, setCounters] = useState(INITIAL_COUNTER_STATE)
-  
   const [clicks, setClicks] = useState([])
 
   const handleClickLeft = () => {
-    const newCounterState = {
-      ...counters,
-      left: counters.left +1,
-      right: counters.right,
-    }
-    setCounters(newCounterState)
-
     setClicks((prevClicks) => [...prevClicks, 'L'])
-
   }
 
   const handleClickRight = () => {
-    setCounters({
-      ...counters,
-      left: counters.left,
-      right: counters.right +1,
-    })
-
-    setClicks((prevClicks) => [...prevClicks, 'R'] )
+    setClicks((prevClicks) => [...prevClicks, 'R'])
   }
 
   const handleReset = () => {
-    setCounters(INITIAL_COUNTER_STATE)
-
     setClicks([])
   }
 
+  const left = clicks.filter(click => click === 'L')
+  const right = clicks.filter(click => click === 'R')
+
   return (
     <div>
-      {counters.left}
+      {left.length}
       <button onClick={handleClickLeft}>
         left
       </button>
       <button onClick={handleClickRight}>
         right
       </button>
-      {counters.right}
+      {right.length}
+     
       <br/>
       <button onClick={handleReset}>
         reset
       </button>
       <br/>
       <h3>Clicks totales: {clicks.length}</h3>
-      {counters.mensaje}
       <br/>
       {clicks.length === 0 ? <WarningNotUsed /> : <ListOfClicks clicks={clicks} />}
     </div>
